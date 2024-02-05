b34132f77361bf03871fbd13230895b01b323fec
mojtaba <mojtaba.mohamed187@gmail.com>
2024-02-06 00:05:29 +0200

    fix api response issue and improve code by remove repeated parts

--- js/script.js
+++ js/script.js
@@ -1,65 +1,57 @@
-let inputCity = document.querySelector(".search-input");
-let searchBtn = document.querySelector(".search-btn");
-let cityName = document.querySelector(".city-name");
-let temp = document.querySelector(".temp");
-let weatherDescription = document.querySelector(".weather-description");
-let humidity = document.querySelector(".humidity");
-let windSpeed = document.querySelector(".wind-speed");
-let visibility = document.querySelector(".visibility");
-let icon = document.querySelector(".icon");
-
-const settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://community-open-weather-map.p.rapidapi.com/weather?q=khartoum&lat=0&lon=0&id=2172797&lang=en&units=metric",
-    "method": "GET",
-    "headers": {
-        "X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com",
-        "X-RapidAPI-Key": "a2a5cb1cccmsh88cd62d1ed8b204p1ff3f0jsn2e184c845a9b",
-        "Access-Control-Allow-Origin": "*"
-    }
+const elements = {
+    inputCity: document.querySelector(".search-input"),
+    searchBtn: document.querySelector(".search-btn"),
+    cityName: document.querySelector(".city-name"),
+    temp: document.querySelector(".temp"),
+    weatherDescription: document.querySelector(".weather-description"),
+    humidity: document.querySelector(".humidity"),
+    windSpeed: document.querySelector(".wind-speed"),
+    visibility: document.querySelector(".visibility"),
+    icon: document.querySelector(".icon"),
 };
 
-$.ajax(settings).done(function (response) {
-    display(response)
+const openWeatherSettings = {
+    async: true,
+    crossDomain: true,
+    baseApiUrl: "http://api.openweathermap.org/data/2.5/weather",
+    apiKey: "7174c66a24f42690c9f089cd53652286",
+    units: "metric",
+};
+
+const getWeatherSettings = (city) => ({
+    async: true,
+    crossDomain: true,
+    url: `${openWeatherSettings.baseApiUrl}?q=${city}&id=524901&appid=${openWeatherSettings.apiKey}&units=${openWeatherSettings.units}`,
+    method: "GET",
 });
 
-searchBtn.addEventListener("click", () => {
-    weather();
-});
+$.ajax({
+    ...getWeatherSettings("london"),
+}).done(display);
 
-inputCity.addEventListener("keyup", (event) => {
-    if(event.code == "Enter") {
+elements.searchBtn.addEventListener("click", weather);
+
+elements.inputCity.addEventListener("keyup", (event) => {
+    if (event.code === "Enter") {
         weather();
     }
 });
 
 function weather() {
-    const settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://community-open-weather-map.p.rapidapi.com/weather?q="+ inputCity.value +"&lat=0&lon=0&id=2172797&lang=en&units=metric",
-        "method": "GET",
-        "headers": {
-            "X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com",
-            "X-RapidAPI-Key": "e8d5694c29msh0b7491831ea308ap16edf1jsn3f54c881a736",
-            "Access-Control-Allow-Origin": "*"
-        }
-    };
-    
-    $.ajax(settings).done(function (response) {
-        display(response)
-    });
-};
+    const city = elements.inputCity.value;
+    if (city) {
+        $.ajax(getWeatherSettings(city)).done(display);
+    }
+}
 
 function display(response) {
+    const { main, weather, wind, visibility, name } = response;
 
-    cityName.innerHTML = response.name;
-    temp.innerHTML = response.main.temp;
-    weatherDescription.innerHTML = response.weather[0].description;
-    humidity.innerHTML = "Humidity : " + response.main.humidity + "%";
-    windSpeed.innerHTML = "Wind Speed : " + response.wind.speed + " m/s";
-    visibility.innerHTML = "Visibility : " + response.visibility / 1000;
-    icon.src = "http://openweathermap.org/img/wn/"+ response.weather[0].icon + "@2x.png";
-
-};
+    elements.cityName.innerHTML = name;
+    elements.temp.innerHTML = main.temp;
+    elements.weatherDescription.innerHTML = weather[0].description;
+    elements.humidity.innerHTML = `Humidity: ${main.humidity}%`;
+    elements.windSpeed.innerHTML = `Wind Speed: ${wind.speed} m/s`;
+    elements.visibility.innerHTML = `Visibility: ${visibility / 1000}`;
+    elements.icon.src = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+}
